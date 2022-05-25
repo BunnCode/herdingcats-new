f64493575a7ea188bfa61f2bd30e033678086048
Austin-Mello <60442413+Austin-Mello@users.noreply.github.com>
2022-05-25 10:59:42 -0700

    Tracked down a bug that made the jump strength inconsistent.  Maybe.

--- Assets/HerdingCats/Scripts/UI/PlayerMovement.cs
+++ Assets/HerdingCats/Scripts/UI/PlayerMovement.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
     public KeyCode JumpKeyCode = KeyCode.Space;
-    public float JumpStrength = 1;
+    public float JumpStrength;
     public CharacterController controller;
 
     public float speed = 12f;
