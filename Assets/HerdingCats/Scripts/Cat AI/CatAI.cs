713395ce3efa63d114bbc873b08de568e43eb8dc
griffin-schumock <schumockg@gmail.com>
2022-06-02 21:33:26 -0700

    Removing Third Party Indicator

--- Assets/HerdingCats/Scripts/Cat AI/CatAI.cs
+++ Assets/HerdingCats/Scripts/Cat AI/CatAI.cs
@@ -144,7 +144,6 @@ public class CatAI : MonoBehaviour {
     /// Set default state.  Once this calls, the AI loop begins!
     /// </summary>
     private void Start() {
-        this.GetComponent<Target>().enabled = false;
         curiosity = Random.Range(0, 90);
         setState(CatState.ROAMING);
         _billboardController = GetComponentInChildren<MultiBillboard>();
@@ -218,11 +217,9 @@ public class CatAI : MonoBehaviour {
         //Trigger appropriate behavior
         switch (currentState) {
             case CatState.ROAMING:
-                this.GetComponent<Target>().enabled = false;
                 StartCoroutine(roam());
                 break;
             case CatState.CURIOUS:
-                this.GetComponent<Target>().enabled = true;
                 StartCoroutine(ApproachHazard());
                 break;
             case CatState.DISTRESS:
